8260407b1d4918c2a5443231ff834af98cf89d3c
AshKemp <kmuraliashwin@gmail.com>
2024-06-23 12:18:44 +0530

    1. install zod validator 2. create routes for get and delete an expense 3. infer the type of expense from zod

--- routes/expenses.ts
+++ routes/expenses.ts
@@ -1,16 +1,17 @@
 import { Hono } from "hono";
 import { z } from "zod";
+import { zValidator } from "@hono/zod-validator";
+import { visitCommaListElements } from "typescript";
 
-type Expense = {
-  id: number;
-  title: string;
-  amount: number;
-};
-
-const createPostSchema = z.object({
-  title: z.string(),
-  amount: z.number(),
+const expenseSchema = z.object({
+  id: z.number().int().positive().min(1),
+  title: z.string().min(3).max(100),
+  amount: z.number().int().positive(),
 });
+
+const createPostSchema = expenseSchema.omit({ id: true });
+
+type Expense = z.infer<typeof expenseSchema>;
 
 const fakeExpenses: Expense[] = [
   { id: 1, title: "Groceries", amount: 50 },
@@ -22,9 +23,27 @@ export const expensesRoute = new Hono()
   .get("/", async (c) => {
     return c.json({ expenses: fakeExpenses });
   })
-  .post("/", async (c) => {
-    const data = await c.req.json();
-    const expense = createPostSchema.parse(data);
-    console.log({ expense });
+  .post("/", zValidator("json", createPostSchema), async (c) => {
+    const expense = await c.req.valid("json");
+    fakeExpenses.push({ ...expense, id: fakeExpenses.length + 1 });
+    c.status(201);
     return c.json(expense);
+  })
+  .get("/:id{[0-9]+}", (c) => {
+    const id = Number.parseInt(c.req.param("id"));
+    const expense = fakeExpenses.find((expense) => expense.id === id);
+    if (!expense) {
+      return c.notFound();
+    }
+    return c.json({ expense });
+  })
+  .delete("/:id{[0-9]+}", (c) => {
+    const id = Number.parseInt(c.req.param("id"));
+    const index = fakeExpenses.findIndex((expense) => expense.id === id);
+
+    if (index === -1) {
+      return c.notFound();
+    }
+    const deletedExpense = fakeExpenses.splice(index, 1)[0];
+    return c.json({ expense: deletedExpense });
   });
